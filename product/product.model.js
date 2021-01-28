644e7962898c4a3468942313e787c41ab0ff438e
Obi James Onyemaechi <james-softprism@Obis-MacBook-Pro.local>
2021-01-28 18:12:07 +0100

    updated product availability to boolean type

--- product/product.model.js
+++ product/product.model.js
@@ -1,14 +1,14 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const schema = new Schema({
+const Productschema = new Schema({
     product_name: { type: String, required: true },
     product_image: [{ type: String, required: true }], // array to store multiple images
     store: { type: Schema.Types.ObjectId, ref: "Store", required: true },
     category: { type: Schema.Types.ObjectId, ref: "Category", required: true },
-    availability: { type: String, required: true }, // "soldout","onsales"
+    availability: { type: Boolean, required: true },
     price: {type: String, required: true},
     createdDate: { type: Date, default: Date.now }
 });
 
-module.exports = mongoose.model('Product', schema);
+module.exports = mongoose.model('Product', Productschema);
