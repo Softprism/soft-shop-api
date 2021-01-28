0d0a461b438e64aed96b7963bb970700eefbf373
Obi James Onyemaechi <james-softprism@Obis-MacBook-Pro.local>
2021-01-28 10:48:33 +0100

    added address array to user model

--- user/user.model.js
+++ user/user.model.js
@@ -5,6 +5,7 @@ const schema = new Schema({
     first_name: { type: String, required: true },
     last_name: { type: String, required: true },
     phone_number: { type: String, required: true },
+    address: [{ type: String, required: true }],
     email: { type: String, unique: true, required: true },
     password: { type: String, required: true },
     orders: [{ type: Schema.Types.ObjectId, ref: "Order"}], // array to store multiple orders
