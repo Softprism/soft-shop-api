5c10b7c8dc585b7ec7168f8c09fe68b0158391c1
Laurent <o.sherkes@gmail.com>
2021-01-31 18:28:35 +0100

    Added check for if address is empty

--- user/user.service.js
+++ user/user.service.js
@@ -154,17 +154,20 @@ const updateUser = async (req, res) => {
 
 		if (!user) return res.status(404).json({ msg: 'User not found' });
 
-		// Check if address array is not empty
-		if (!user.address.length < 1) {
-			// Set the address value in user object to address found from db, then append new address
-			userFields.address = [...user.address, address];
+		// Check if address field is not empty
+		if (address !== '' || null) {
+			if (!user.address.length < 1) {
+				// Check if address array is not empty
+				// Set the address value in user object to address found from db, then append new address
+				userFields.address = [...user.address, address];
+			}
 		}
 
 		// Updates the user Object with the changed values
 		user = await User.findByIdAndUpdate(
 			req.params.id,
 			{ $set: userFields },
-			{ new: true }
+			{ new: true, useFindAndModify: true }
 		);
 
 		res.json(user);
