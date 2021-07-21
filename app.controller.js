4d92a2dcd3bab474fcd4c5440b247d32e8c0b68b
Obi James Onyemaechi <jamesobi@softprism.org>
2021-07-21 23:03:14 +0100

    testing automatic deployment10

--- app.controller.js
+++ app.controller.js
@@ -5,7 +5,10 @@ router.get('/', testServer);
 module.exports = router;
 
 function testServer(req, res) {
-  res.json('api server is online')
+  res.json({
+    status: true,
+    msg: 'server is online'
+  })
   // this is just here to verify if the server is online
   //doesn't require JWT
 }
