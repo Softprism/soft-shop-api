edd12a0dee0641ad1982d42dc820fe04f3ea7089
Obi James Onyemaechi <jamesobi@softprism.org>
2021-07-23 15:20:06 +0100

    updated store model

--- store/store.model.js
+++ store/store.model.js
@@ -5,7 +5,7 @@ const StoreSchema = mongoose.Schema({
 	images: [{ type: String, required: true }], // array to store multiple images
 	address: { type: String, required: true },
 	phone_number: { type: String, required: true },
-	email: { type: String, required: true },
+	email: { type: String, unique: true, required: true },
 	password: { type: String, required: true },
 	rating: { type: String, required: false },
 	createdDate: { type: Date, default: Date.now },
