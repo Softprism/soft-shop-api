a1a7dade6dbbfeabad86dbfe5a3c68eab0cb4f4d
Laurent <o.sherkes@gmail.com>
2021-07-24 09:07:21 +0100

    Minor Fixes to Indentation

--- order/order.controller.js
+++ order/order.controller.js
@@ -10,179 +10,203 @@ const { check, validationResult } = require('express-validator');
 
 router.get('/', auth, getOrders);
 router.post(
-    '/create',
-    auth,
-    [
+	'/create',
+	auth,
+	[
 		check('product_meta', 'error with product data ').isLength({ min: 1 }),
-        check('store', 'Please select a store').not().isEmpty(),
-        check('user', 'user field missing').not().isEmpty(),
-        check('status', 'status field missing').not().isEmpty()
+		check('store', 'Please select a store').not().isEmpty(),
+		check('user', 'user field missing').not().isEmpty(),
+		check('status', 'status field missing').not().isEmpty(),
 	],
-    createOrder
-)
-router.put('/add_favorite/:orderID', auth, addFavorite)
-router.get('/get_order_details/:orderID', auth, getOrderDetails)
-router.get('/get_favorites/:userID', auth, getFavorites)
-router.get('/user_order_history/:userID', auth, getOrderHistory)
-router.get('/store_order_history/:storeID', auth, getStoreOrderHistory)
-router.get('/get_user_cart/:userID', auth, getCartItems)
-router.put('/edit_user_order/:orderID', auth, editOrder)
-router.put('/cancel_user_order/:orderID', auth, cancelOrder)
-router.put('/deliver_order/:orderID', auth, deliverOrder)
-router.put('/receive_order/:orderID', auth, receiveOrder)
-router.put('/complete_order/:orderID', auth, completeOrder)
-module.exports = router;
+	createOrder
+);
+
+router.put('/add_favorite/:orderID', auth, addFavorite);
+router.get('/get_order_details/:orderID', auth, getOrderDetails);
+router.get('/get_favorites/:userID', auth, getFavorites);
+router.get('/user_order_history/:userID', auth, getOrderHistory);
+router.get('/store_order_history/:storeID', auth, getStoreOrderHistory);
+router.get('/get_user_cart/:userID', auth, getCartItems);
+router.put('/edit_user_order/:orderID', auth, editOrder);
+router.put('/cancel_user_order/:orderID', auth, cancelOrder);
+router.put('/deliver_order/:orderID', auth, deliverOrder);
+router.put('/receive_order/:orderID', auth, receiveOrder);
+router.put('/complete_order/:orderID', auth, completeOrder);
 
 //======================================================================
 
 function getOrders(req, res, next) {
-    orderService.getOrders()
-        .then(orders => res.json(orders))
-        .catch(err => next(err));
+	orderService
+		.getOrders()
+		.then((orders) => res.json(orders))
+		.catch((err) => next(err));
 }
 
 //======================================================================
 
-function createOrder(req,res,next) {
-    const errors = validationResult(req);
+function createOrder(req, res, next) {
+	const errors = validationResult(req);
 
 	if (!errors.isEmpty()) {
 		return res.status(400).json({ errors: errors.array() });
-    }
- 
-    orderService.createOrder(req.body)
-    .then(()=>res.json({
-        success: true,
-        message: 'order created',
-        
-    }))
-    .catch(err => next(err))
+	}
+
+	orderService
+		.createOrder(req.body)
+		.then(() =>
+			res.json({
+				success: true,
+				message: 'order created',
+			})
+		)
+		.catch((err) => next(err));
 }
 
 //======================================================================
 
-function addFavorite(req,res,next) {
-    orderService.addFavorite(req.params.orderID)
-    .then(()=>res.json({
-        success: true,
-    }))
-    .catch(err => next(err))
+function addFavorite(req, res, next) {
+	orderService
+		.addFavorite(req.params.orderID)
+		.then(() =>
+			res.json({
+				success: true,
+			})
+		)
+		.catch((err) => next(err));
 }
 
 //======================================================================
 
-function getOrderDetails(req,res,next) {
-
-    orderService
-    .getOrderDetails(req.params.orderID)
-        .then(result => res.json({
-            success: true,
-            message: result,
-        }))
-        .catch(err => next(err))
+function getOrderDetails(req, res, next) {
+	orderService
+		.getOrderDetails(req.params.orderID)
+		.then((result) =>
+			res.json({
+				success: true,
+				message: result,
+			})
+		)
+		.catch((err) => next(err));
 }
 
 //======================================================================
 
-function getOrderHistory(req,res,next) {
-    
-    orderService
-        .getOrderHistory(req.params.userID)
-            .then(orders => res.json({
-                success: true,
-                message: orders,
-            }))
-            .catch(err => next(err))
+function getOrderHistory(req, res, next) {
+	orderService
+		.getOrderHistory(req.params.userID)
+		.then((orders) =>
+			res.json({
+				success: true,
+				message: orders,
+			})
+		)
+		.catch((err) => next(err));
 }
 
 //======================================================================
 
-function getStoreOrderHistory(req,res,next) {
-    
-    orderService
-        .getStoreOrderHistory(req.params.storeID)
-            .then(orders => res.json({
-                success: true,
-                message: orders,
-            }))
-            .catch(err => next(err))
+function getStoreOrderHistory(req, res, next) {
+	orderService
+		.getStoreOrderHistory(req.params.storeID)
+		.then((orders) =>
+			res.json({
+				success: true,
+				message: orders,
+			})
+		)
+		.catch((err) => next(err));
 }
 
 //======================================================================
-function getCartItems(req,res,next) {
-
-    orderService.getCartItems(req.params.userID)
-        .then(items => res.json({
-            success: true,
-            message: items
-        }))
-        .catch(err => next(err))
+function getCartItems(req, res, next) {
+	orderService
+		.getCartItems(req.params.userID)
+		.then((items) =>
+			res.json({
+				success: true,
+				message: items,
+			})
+		)
+		.catch((err) => next(err));
 }
 
 //======================================================================
 
 function editOrder(req, res, next) {
-
-    orderService.editOrder(req.params.orderID,req.body)
-        .then(() => res.json({
-            success: true
-        }))
-        .catch(err => next(err))
+	orderService
+		.editOrder(req.params.orderID, req.body)
+		.then(() =>
+			res.json({
+				success: true,
+			})
+		)
+		.catch((err) => next(err));
 }
 
 //======================================================================
 
 function cancelOrder(req, res, next) {
-
-    orderService.cancelOrder(req.params.orderID)
-        .then(() => res.json({
-            success: true
-        }))
-        .catch(err => next(err))
+	orderService
+		.cancelOrder(req.params.orderID)
+		.then(() =>
+			res.json({
+				success: true,
+			})
+		)
+		.catch((err) => next(err));
 }
 
 //======================================================================
 
 function completeOrder(req, res, next) {
-
-    orderService.completeOrder(req.params.orderID)
-        .then(() => res.json({
-            success: true
-        }))
-        .catch(err => next(err))
+	orderService
+		.completeOrder(req.params.orderID)
+		.then(() =>
+			res.json({
+				success: true,
+			})
+		)
+		.catch((err) => next(err));
 }
 
 //======================================================================
 
 function receiveOrder(req, res, next) {
-
-    orderService.receiveOrder(req.params.orderID)
-        .then(() => res.json({
-            success: true
-        }))
-        .catch(err => next(err))
+	orderService
+		.receiveOrder(req.params.orderID)
+		.then(() =>
+			res.json({
+				success: true,
+			})
+		)
+		.catch((err) => next(err));
 }
 
 //======================================================================
 
 function deliverOrder(req, res, next) {
-
-    orderService.deliverOrder(req.params.orderID)
-        .then(() => res.json({
-            success: true
-        }))
-        .catch(err => next(err))
+	orderService
+		.deliverOrder(req.params.orderID)
+		.then(() =>
+			res.json({
+				success: true,
+			})
+		)
+		.catch((err) => next(err));
 }
 
 //======================================================================
-function getFavorites(req,res,next) {
-    orderService
-        .getFavorites(req.params.userID)
-            .then(orders => res.json({
-                success: true,
-                message: orders
-            }))
-            .catch(err => next(err))
+function getFavorites(req, res, next) {
+	orderService
+		.getFavorites(req.params.userID)
+		.then((orders) =>
+			res.json({
+				success: true,
+				message: orders,
+			})
+		)
+		.catch((err) => next(err));
 }
 
 //======================================================================
+module.exports = router;
--- product/product.controller.js
+++ product/product.controller.js
@@ -25,16 +25,17 @@ router.get('/:id', auth, getStoreProducts);
 // @desc    Create product
 // @access  Private
 router.post(
-    '/create', [
-        check('product_name', 'Please Enter Product Name').not().isEmpty(),
-        // check('images', 'Please add images for your store').not().isEmpty(),
-        check('category', 'Please select Category').not().isEmpty(),
-        check('availability', 'Please select availability status').not().isEmpty(),
-        check('price', 'Please enter price of product').not().isEmpty(),
-        // check('rating', 'Please Enter Stores Address').not().isEmpty(),
-    ],
-    auth,
-    createProduct
+	'/create',
+	[
+		check('product_name', 'Please Enter Product Name').not().isEmpty(),
+		// check('images', 'Please add images for your store').not().isEmpty(),
+		check('category', 'Please select Category').not().isEmpty(),
+		check('availability', 'Please select availability status').not().isEmpty(),
+		check('price', 'Please enter price of product').not().isEmpty(),
+		// check('rating', 'Please Enter Stores Address').not().isEmpty(),
+	],
+	auth,
+	createProduct
 );
 
 // @route   PUT /product/update/:id
@@ -42,120 +43,146 @@ router.post(
 // @access  Private
 router.put('/update/:id', auth, updateProduct);
 
-
 // @route   DELETE /product/delete/:id
 // @desc    Delete product
 // @access  Private
 router.delete('/delete/:id', auth, deleteProduct);
-router.get("/find/:searchParam", findProduct)
+router.get('/find/:searchParam', findProduct);
 
 module.exports = router;
 
 function getProducts(req, res, next) {
-    productService.getProducts()
-        .then(products => res.json({
-            success: true,
-            result: products
-        }))
-        .catch(err => res.status(400).send({
-            success: false,
-            message: err
-        }));
+	productService
+		.getProducts()
+		.then((products) =>
+			res.json({
+				success: true,
+				result: products,
+			})
+		)
+		.catch((err) =>
+			res.status(400).send({
+				success: false,
+				message: err,
+			})
+		);
 }
 
 function getMyProducts(req, res, next) {
-    productService.getMyProducts(req.user.id)
-        .then(products => res.json({
-            success: true,
-            products: products
-        }))
-        .catch(err => res.status(500).send({
-            success: false,
-            message: err
-        }));
+	productService
+		.getMyProducts(req.user.id)
+		.then((products) =>
+			res.json({
+				success: true,
+				products: products,
+			})
+		)
+		.catch((err) =>
+			res.status(500).send({
+				success: false,
+				message: err,
+			})
+		);
 }
 
 function getStoreProducts(req, res, next) {
-    productService.getStoreProducts(req.params.id)
-        .then(products => res.json({
-            success: true,
-            products: products
-        }))
-        .catch(err => res.status(500).send({
-            success: false,
-            message: err
-        }));
+	productService
+		.getStoreProducts(req.params.id)
+		.then((products) =>
+			res.json({
+				success: true,
+				products: products,
+			})
+		)
+		.catch((err) =>
+			res.status(500).send({
+				success: false,
+				message: err,
+			})
+		);
 }
 
 function createProduct(req, res, next) {
-    const errors = validationResult(req);
+	const errors = validationResult(req);
 
-    if (!errors.isEmpty()) {
-        let error_msgs = [];
-        errors.array().forEach(element => {
-            error_msgs = [...error_msgs, element.msg]
-        });
-        return res.status(400).json({
-            success: false,
-            errors: error_msgs
-        });
-    }
+	if (!errors.isEmpty()) {
+		let error_msgs = [];
+		errors.array().forEach((element) => {
+			error_msgs = [...error_msgs, element.msg];
+		});
+		return res.status(400).json({
+			success: false,
+			errors: error_msgs,
+		});
+	}
 
-    productService.createProduct(req.body, req.user.id)
-        .then(product => {
-            res.json({
-                success: true,
-                product: product
-            });
-        })
-        .catch(err => res.status(err.code != null ? err.code : 500).send({
-            success: false,
-            message: err.msg != null ? err.msg : 'Server Error'
-        }));
+	productService
+		.createProduct(req.body, req.user.id)
+		.then((product) => {
+			res.json({
+				success: true,
+				product: product,
+			});
+		})
+		.catch((err) =>
+			res.status(err.code != null ? err.code : 500).send({
+				success: false,
+				message: err.msg != null ? err.msg : 'Server Error',
+			})
+		);
 }
 
 function updateProduct(req, res, next) {
-    // const errors = validationResult(req);
+	// const errors = validationResult(req);
 
-    // if (!errors.isEmpty()) {
-    //     res.status(400).json({ 
-    //         success: false,
-    //         errors: errors.array()['msg']
-    //     });
-    // }
-    console.log(req.params.id);
-    productService.updateProduct(req.body, req.params.id, req.user.id)
-        .then(product => {
-            res.json({
-                success: true,
-                product: product
-            });
-        })
-        .catch(err => res.status(err.code != null ? err.code : 500).send({
-            success: false,
-            message: err.msg != null ? err.msg : 'Server Error'
-        }));
+	// if (!errors.isEmpty()) {
+	//     res.status(400).json({
+	//         success: false,
+	//         errors: errors.array()['msg']
+	//     });
+	// }
+	console.log(req.params.id);
+	productService
+		.updateProduct(req.body, req.params.id, req.user.id)
+		.then((product) => {
+			res.json({
+				success: true,
+				product: product,
+			});
+		})
+		.catch((err) =>
+			res.status(err.code != null ? err.code : 500).send({
+				success: false,
+				message: err.msg != null ? err.msg : 'Server Error',
+			})
+		);
 }
 
 function deleteProduct(req, res, next) {
-    productService.deleteProduct(req.params.id, req.user.id)
-        .then(message => {
-            res.json({
-                success: true,
-                message: message
-            });
-        })
-        .catch(err => res.status(err.code != null ? err.code : 500).send({
-            success: false,
-            message: err.msg != null ? err.msg : 'Server Error'
-        }));
+	productService
+		.deleteProduct(req.params.id, req.user.id)
+		.then((message) => {
+			res.json({
+				success: true,
+				message: message,
+			});
+		})
+		.catch((err) =>
+			res.status(err.code != null ? err.code : 500).send({
+				success: false,
+				message: err.msg != null ? err.msg : 'Server Error',
+			})
+		);
 }
 
-function findProduct( req, res, next) {
-    productService.findProduct(req.params.searchParam)
-    .then(results => res.json({
-        success: true,
-        message: results
-    }))
-    .catch(err => next(err))
+function findProduct(req, res, next) {
+	productService
+		.findProduct(req.params.searchParam)
+		.then((results) =>
+			res.json({
+				success: true,
+				message: results,
+			})
+		)
+		.catch((err) => next(err));
 }
