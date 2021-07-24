de06cd42de3f713f50ef00dc7c72b222b6959bf6
Laurent <o.sherkes@gmail.com>
2021-07-24 09:01:09 +0100

    Minor Fixes to Indentation

--- product/product.service.js
+++ product/product.service.js
@@ -7,136 +7,136 @@ const Product = db.Product;
 const Store = db.Store;
 
 const getProducts = async () => {
-console.log(1)
-  try {
-    //get all products in the db
-    return await Product.find();
-  } catch (error) {
-    console.log(error)
-    throw error
-  }  
-
-}
+	console.log(1);
+	try {
+		//get all products in the db
+		return await Product.find();
+	} catch (error) {
+		console.log(error);
+		throw error;
+	}
+};
 
 async function findProduct(searchParam) {
-    let regex = new RegExp(searchParam, 'i');
-    return await Product.find({ product_name: regex}).exec(); // we'll prioritize results to be the ones closer to the users
+	let regex = new RegExp(searchParam, 'i');
+	return await Product.find({ product_name: regex }).exec(); // we'll prioritize results to be the ones closer to the users
 }
 
 async function getMyProducts(storeId) {
-    return await Product.find({
-        'store': mongoose.Types.ObjectId(storeId)
-    });
+	return await Product.find({
+		store: mongoose.Types.ObjectId(storeId),
+	});
 }
 
 async function getStoreProducts(storeId) {
-    return await Product.find({
-        'store': mongoose.Types.ObjectId(storeId)
-    });
+	return await Product.find({
+		store: mongoose.Types.ObjectId(storeId),
+	});
 }
 
 async function createProduct(productParam, id) {
-    const { product_name, category, availability, price, rating } = productParam;
+	const { product_name, category, availability, price, rating } = productParam;
 
-    let store = await Store.findById(id);
+	let store = await Store.findById(id);
 
-    if (!store) {
-        throw ({
-            code: 400,
-            msg: 'Login your store to create a product'
-        });
-    }
+	if (!store) {
+		throw {
+			code: 400,
+			msg: 'Login your store to create a product',
+		};
+	}
 
-    let products = await getProducts();
+	let products = await getProducts();
 
-    products.forEach(element => {
-        if (element.product_name == product_name) {
-            throw ({
-                code: 400,
-                msg: 'Product already exists'
-            });
-        }
-    });
+	products.forEach((element) => {
+		if (element.product_name == product_name) {
+			throw {
+				code: 400,
+				msg: 'Product already exists',
+			};
+		}
+	});
 
-    newProduct = new Product({
-        product_name: product_name,
-        store: store._id,
-        category: mongoose.Types.ObjectId(category),
-        availability: availability,
-        price: price,
-        rating: rating
-    });
+	newProduct = new Product({
+		product_name: product_name,
+		store: store._id,
+		category: mongoose.Types.ObjectId(category),
+		availability: availability,
+		price: price,
+		rating: rating,
+	});
 
-    await newProduct.save();
-    return newProduct
+	await newProduct.save();
+	return newProduct;
 }
 
 async function updateProduct(productParam, productId, storeId) {
-    const { product_name, category, availability, price, rating } = productParam;
+	const { product_name, category, availability, price, rating } = productParam;
 
-    console.log(productId);
-    let store = await Store.findById(storeId);
-    if (!store) {
-        throw ({
-            code: 400,
-            msg: 'Login your store to create a product'
-        });
-    }
+	console.log(productId);
+	let store = await Store.findById(storeId);
+	if (!store) {
+		throw {
+			code: 400,
+			msg: 'Login your store to create a product',
+		};
+	}
 
-    let product = await Product.findById(productId);
+	let product = await Product.findById(productId);
 
-    if (!product) {
-        throw ({
-            code: 400,
-            msg: 'Product not found'
-        });
-    }
+	if (!product) {
+		throw {
+			code: 400,
+			msg: 'Product not found',
+		};
+	}
 
-    const productUpdate = {};
+	const productUpdate = {};
 
-    if (product_name) productUpdate.product_name = product_name;
-    if (category) productUpdate.category = mongoose.Types.ObjectId(category);
-    if (availability) productUpdate.availability = availability;
-    if (price) productUpdate.price = price;
-    if (rating) productUpdate.rating = rating;
+	if (product_name) productUpdate.product_name = product_name;
+	if (category) productUpdate.category = mongoose.Types.ObjectId(category);
+	if (availability) productUpdate.availability = availability;
+	if (price) productUpdate.price = price;
+	if (rating) productUpdate.rating = rating;
 
-    updatedProduct = await Product.findByIdAndUpdate(
-        productId, { $set: productUpdate }, { new: true, useFindAndModify: true }
-    );
-    return updatedProduct;
+	updatedProduct = await Product.findByIdAndUpdate(
+		productId,
+		{ $set: productUpdate },
+		{ new: true, useFindAndModify: true }
+	);
+	return updatedProduct;
 }
 
 async function deleteProduct(productId, storeId) {
+	let store = await Store.findById(storeId);
 
-    let store = await Store.findById(storeId);
+	if (!store) {
+		throw {
+			code: 400,
+			msg: 'Login your store to create a product',
+		};
+	}
 
-    if (!store) {
-        throw ({
-            code: 400,
-            msg: 'Login your store to create a product'
-        });
-    }
+	let product = await Product.findById(productId);
 
-    let product = await Product.findById(productId);
+	if (!product) {
+		throw {
+			code: 400,
+			msg: 'Product not found',
+		};
+	}
 
-    if (!product) {
-        throw ({
-            code: 400,
-            msg: 'Product not found'
-        });
-    }
+	await Product.deleteOne({ _id: productId });
 
-    await Product.deleteOne({ _id: productId });
-
-    return { message: 'Product deleted Successfully' };
+	return { message: 'Product deleted Successfully' };
 }
 
 module.exports = {
-  getProducts,
-  getMyProducts,
-  getStoreProducts,
-  createProduct,
-  updateProduct,
-  deleteProduct,
-  findProduct
+	getProducts,
+	getMyProducts,
+	getStoreProducts,
+	createProduct,
+	updateProduct,
+	deleteProduct,
+	findProduct,
 };
