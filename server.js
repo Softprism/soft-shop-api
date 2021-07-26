0125cfb40f23c1f7f0346b77621f9b58742585d7
Obi James Onyemaechi <jamesobi@softprism.org>
2021-07-26 03:08:40 +0100

    fixing package.json for heroku

--- server.js
+++ server.js
@@ -48,7 +48,7 @@ app.use(errorHandler);
 
 // Start server
 const PORT = process.env.PORT || 5000;
-
+console.log(process.env.JWT_SECRET)
 app.listen(
 	PORT,
 	console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
