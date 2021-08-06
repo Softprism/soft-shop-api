dfb48cc1fad235703431825f256bd923efd8780c
Obi James Onyemaechi <jamesobi@softprism.org>
2021-08-06 13:51:53 +0100

    adds review model

---
+++ models/review.model.js
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose';
+
+const ReviewSchema = mongoose.Schema({
+  star: {type: Number},
+	user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+	product: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true },
+  text: {type: String}
+})
+
+const Review = mongoose.model('Review', ReviewSchema);
+
+export default Review;
