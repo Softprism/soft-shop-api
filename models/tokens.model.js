702ca5da249df7d2d0aa8759d487689c1c513518
Obi James Onyemaechi <jamesobi@softprism.org>
2021-10-19 21:38:19 +0100

    Feat: Add Token Model

---
+++ models/tokens.model.js
@@ -0,0 +1,31 @@
+import mongoose from 'mongoose';
+
+const tokenSchema = mongoose.Schema(
+	{
+		email: {
+			type: String,
+			required: true,
+			ref: 'User',
+		},
+		token: {
+			type: String,
+			required: true,
+		},
+    type: {
+      type: String,
+      required: true
+    },
+		createdAt: {
+			type: Date,
+			default: Date.now,
+			expires: 3600,
+		},
+	},
+	{
+		timestamps: true,
+	}
+);
+
+const Token = mongoose.model('Token', tokenSchema);
+
+export default Token;
