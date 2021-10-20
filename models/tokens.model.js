6ffa3519a86287749b9f7aafb74642cfe5d782c3
Obi James Onyemaechi <jamesobi@softprism.org>
2021-10-20 17:45:06 +0100

    chore: Change token field to otp
    To avoid confusions, the keyword Token will be used to represent the model while OTP is the code required to authenticate requests.

--- models/tokens.model.js
+++ models/tokens.model.js
@@ -1,31 +1,31 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const tokenSchema = mongoose.Schema(
-	{
-		email: {
-			type: String,
-			required: true,
-			ref: 'User',
-		},
-		token: {
-			type: String,
-			required: true,
-		},
+  {
+    email: {
+      type: String,
+      required: true,
+      ref: "User",
+    },
+    otp: {
+      type: String,
+      required: true,
+    },
     type: {
       type: String,
-      required: true
+      required: true,
     },
-		createdAt: {
-			type: Date,
-			default: Date.now,
-			expires: 3600,
-		},
-	},
-	{
-		timestamps: true,
-	}
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      expires: 3600,
+    },
+  },
+  {
+    timestamps: true,
+  }
 );
 
-const Token = mongoose.model('Token', tokenSchema);
+const Token = mongoose.model("Token", tokenSchema);
 
 export default Token;
