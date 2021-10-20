f84dec0438af94438e93e2a895bfaa297baac4c4
Obi James Onyemaechi <jamesobi@softprism.org>
2021-10-20 18:42:30 +0100

    Feat: Add send token for new user signup email verification

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,43 +1,49 @@
-import express from 'express';
-import { check } from 'express-validator';
+import express from "express";
+import { check } from "express-validator";
 
 const router = express.Router();
 
 import {
-	getUsers,
-	registerUser,
-	loginUser,
-	getLoggedInUser,
-	updateUser,
-	addItemToCart,
-	forgotPassword,
-	validateToken,
-	createNewPassword,
-} from '../controllers/user.controller.js';
+  getUsers,
+  registerUser,
+  loginUser,
+  getLoggedInUser,
+  updateUser,
+  addItemToCart,
+  forgotPassword,
+  validateToken,
+  createNewPassword,
+  verifyEmailAddress,
+} from "../controllers/user.controller.js";
 
-import { auth } from '../middleware/auth.js';
+import { auth } from "../middleware/auth.js";
 
 // @route   GET /users
 // @desc    Get all Users
 // @access  Public
-router.get('/', getUsers);
+router.get("/", getUsers);
+
+// @route   POST /verify
+// @desc    send OTP to verify new user signup email
+// @access  Public
+router.post("/verify", verifyEmailAddress);
 
 // @route   POST /users/register
 // @desc    Register a User
 // @access  Public
 router.post(
-	'/register',
-	[
-		check('first_name', 'Please Enter First Name').not().isEmpty(),
-		check('last_name', 'Please Enter Last Name').not().isEmpty(),
-		check('email', 'Please Enter Valid Email').isEmail(),
-		check('phone_number', 'Please Enter Valid Phone Number').isMobilePhone(),
-		check(
-			'password',
-			'Please Enter Password with 6 or more characters'
-		).isLength({ min: 6 }),
-	],
-	registerUser
+  "/register",
+  [
+    check("first_name", "Please Enter First Name").not().isEmpty(),
+    check("last_name", "Please Enter Last Name").not().isEmpty(),
+    check("email", "Please Enter Valid Email").isEmail(),
+    check("phone_number", "Please Enter Valid Phone Number").isMobilePhone(),
+    check(
+      "password",
+      "Please Enter Password with 6 or more characters"
+    ).isLength({ min: 6 }),
+  ],
+  registerUser
 );
 
 // @route   POST /user/login
@@ -45,47 +51,47 @@ router.post(
 // @access  Public
 
 router.post(
-	'/login',
-	[
-		check('email', 'Please enter a Valid Email').isEmail(),
-		check('password', 'Password should be 6 characters or more').isLength({
-			min: 6,
-		}),
-		check('password', 'Password is Required').exists(),
-	],
-	loginUser
+  "/login",
+  [
+    check("email", "Please enter a Valid Email").isEmail(),
+    check("password", "Password should be 6 characters or more").isLength({
+      min: 6,
+    }),
+    check("password", "Password is Required").exists(),
+  ],
+  loginUser
 );
 
 // @route   GET /user/login
 // @desc    Get logged in user
 // @access  Private
 
-router.get('/login', auth, getLoggedInUser);
+router.get("/login", auth, getLoggedInUser);
 
 // @route   PUT user/
 // @desc    Update User Details
 // @access  Private
 
-router.put('/', auth, updateUser);
+router.put("/", auth, updateUser);
 
 // @route   Put /cart
 // @desc    adds a product tp User's cart
 // @access  Public
-router.put('/cart', auth, addItemToCart);
+router.put("/cart", auth, addItemToCart);
 
 // @route   PUT /password
 // @desc    reset a forget password
 // @access  Public
-router.put('/forgot-password', forgotPassword);
+router.put("/forgot-password", forgotPassword);
 
 // @route   GET /token
 // @desc    validates a token
 // @access  Public
-router.get('/token', validateToken);
+router.get("/token", validateToken);
 
 // @route   PATCH /password
 // @desc    creates new password for user after forget password
 // @access  Public
-router.patch('/password', createNewPassword);
+router.patch("/password", createNewPassword);
 
 export default router;
--- services/user.service.js
+++ services/user.service.js
@@ -1,384 +1,432 @@
-import jwt from 'jsonwebtoken';
-import bcrypt from 'bcryptjs';
-import mongoose from 'mongoose';
+import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs";
+import mongoose from "mongoose";
 
-import User from '../models/user.model.js';
-import Product from '../models/product.model.js';
-import Token from '../models/tokens.model.js';
+import User from "../models/user.model.js";
+import Product from "../models/product.model.js";
+import Token from "../models/tokens.model.js";
 
-import { sendEmail } from '../utils/sendMail.js';
-import otpGenerator from 'otp-generator';
+import { sendEmail } from "../utils/sendMail.js";
+import otpGenerator from "otp-generator";
 
 // Get all Users
 const getUsers = async (urlParams) => {
-	try {
-		let userWithCartItems = [];
-		const limit = Number(urlParams.limit);
-		const skip = Number(urlParams.skip);
-		const cartLength = Number(urlParams.cart);
-		delete urlParams.limit;
-		delete urlParams.skip;
-		delete urlParams.cart;
-		const users = await User.find(urlParams)
-			.select('-password')
-			.sort({ createdDate: -1 }) // -1 for descending sort
-			.populate({
-				path: 'cart.product_id',
-				select: 'product_name price availability',
-			})
-			.populate({ path: 'orders', select: 'orderId status' })
-			.limit(limit)
-			.skip(skip);
+  try {
+    let userWithCartItems = [];
+    const limit = Number(urlParams.limit);
+    const skip = Number(urlParams.skip);
+    const cartLength = Number(urlParams.cart);
+    delete urlParams.limit;
+    delete urlParams.skip;
+    delete urlParams.cart;
+    const users = await User.find(urlParams)
+      .select("-password")
+      .sort({ createdDate: -1 }) // -1 for descending sort
+      .populate({
+        path: "cart.product_id",
+        select: "product_name price availability",
+      })
+      .populate({ path: "orders", select: "orderId status" })
+      .limit(limit)
+      .skip(skip);
 
-		if (cartLength >= 0) {
-			users.forEach((user) => {
-				if (user.cart.length == cartLength) {
-					userWithCartItems.push(user);
-				}
-			});
-			return userWithCartItems;
-		} else {
-			return users;
-		}
-		// return users;
-	} catch (err) {
-		return err;
-	}
+    if (cartLength >= 0) {
+      users.forEach((user) => {
+        if (user.cart.length == cartLength) {
+          userWithCartItems.push(user);
+        }
+      });
+      return userWithCartItems;
+    } else {
+      return users;
+    }
+    // return users;
+  } catch (err) {
+    return err;
+  }
+};
+
+// Verify user email before sign up
+const verifyEmailAddress = async ({ email }) => {
+  try {
+    // check if user exists
+    let user = await User.findOne({ email });
+    if (user) {
+      throw { err: "User with this email already exists" };
+    }
+
+    //Check if request has been made before, can be used for resend token
+    let oldTokenRequest = await Token.findOne({
+      email,
+      type: "user-signup",
+    });
+
+    if (oldTokenRequest) {
+      // resend old token
+      let email_subject = "OTP For Account Creeation";
+      await sendEmail(email, email_subject, oldTokenRequest.token);
+
+      return 0;
+    }
+
+    // generate OTP
+    let otp = otpGenerator.generate(4, {
+      alphabets: false,
+      upperCase: false,
+      specialChars: false,
+    });
+
+    // add token to DB
+    let tokenData = {
+      email,
+      token: otp,
+      type: "user-signup",
+    };
+
+    await new Token(tokenData).save();
+
+    // send otp
+    let email_subject = "OTP For Account Creeation";
+    let email_message = otp;
+    await sendEmail(email, email_subject, email_message);
+
+    return 1;
+  } catch (err) {
+    return err;
+  }
 };
 
 // Register User
 const registerUser = async (userParam) => {
-	const { first_name, last_name, email, phone_number, password } = userParam;
+  const { first_name, last_name, email, phone_number, password } = userParam;
 
-	try {
-		let user = await User.findOne({ email });
+  try {
+    let user = await User.findOne({ email });
 
-		if (user) {
-			// return res
-			// 	.status(400)
-			// 	.json({ msg: 'User with this email already exists' });
-			throw { err: 'User with this email already exists' };
-		}
+    if (user) {
+      throw { err: "User with this email already exists" };
+    }
 
-		// Create User Object
-		user = new User({
-			first_name,
-			last_name,
-			email,
-			phone_number,
-			password,
-		});
+    // Create User Object
+    user = new User({
+      first_name,
+      last_name,
+      email,
+      phone_number,
+      password,
+    });
 
-		const salt = await bcrypt.genSalt(10);
+    const salt = await bcrypt.genSalt(10);
 
-		// Replace password from user object with encrypted one
-		user.password = await bcrypt.hash(password, salt);
+    // Replace password from user object with encrypted one
+    user.password = await bcrypt.hash(password, salt);
 
-		// Save user to db
-		await user.save();
+    // Save user to db
+    await user.save();
 
-		// Define payload for token
-		const payload = {
-			user: {
-				id: user.id,
-			},
-		};
+    // Define payload for token
+    const payload = {
+      user: {
+        id: user.id,
+      },
+    };
 
-		// Generate and return token to server
-		const token = jwt.sign(payload, process.env.JWT_SECRET, {
-			expiresIn: 36000,
-		});
+    // Generate and return token to server
+    const token = jwt.sign(payload, process.env.JWT_SECRET, {
+      expiresIn: 36000,
+    });
 
-		// unset user pass****d
-		user.password = undefined;
+    // unset user pass****d
+    user.password = undefined;
 
-		// set user token
-		user.set('token', token, { strict: false });
+    // set user token
+    user.set("token", token, { strict: false });
 
-		return user;
-	} catch (err) {
-		return err;
-	}
+    return user;
+  } catch (err) {
+    return err;
+  }
 };
 
 // Login User
 const loginUser = async (loginParam) => {
-	const { email, password } = loginParam;
+  const { email, password } = loginParam;
 
-	try {
-		// Find user with email
-		let user = await User.findOne({ email });
+  try {
+    // Find user with email
+    let user = await User.findOne({ email });
 
-		if (!user) {
-			throw { err: 'User not found' };
-		}
+    if (!user) {
+      throw { err: "User not found" };
+    }
 
-		// Check if password matches with stored hash
-		const isMatch = await bcrypt.compare(password, user.password);
+    // Check if password matches with stored hash
+    const isMatch = await bcrypt.compare(password, user.password);
 
-		if (!isMatch) {
-			throw { err: 'Wrong password' };
-		}
+    if (!isMatch) {
+      throw { err: "Wrong password" };
+    }
 
-		// unset user pass***d
-		user.password = undefined;
+    // unset user pass***d
+    user.password = undefined;
 
-		// Define payload for token
-		const payload = {
-			user: {
-				id: user.id,
-			},
-		};
+    // Define payload for token
+    const payload = {
+      user: {
+        id: user.id,
+      },
+    };
 
-		// Generate and return token to server
-		const token = jwt.sign(payload, process.env.JWT_SECRET, {
-			expiresIn: 36000,
-		});
+    // Generate and return token to server
+    const token = jwt.sign(payload, process.env.JWT_SECRET, {
+      expiresIn: 36000,
+    });
 
-		if (!token) {
-			throw { err: 'Missing Token' };
-		}
+    if (!token) {
+      throw { err: "Missing Token" };
+    }
 
-		const pipeline = [
-			{ $unset: ['userReviews', 'userOrders', 'cart', 'password', 'orders'] },
-		];
+    const pipeline = [
+      { $unset: ["userReviews", "userOrders", "cart", "password", "orders"] },
+    ];
 
-		const userDetails = User.aggregate()
-			.match({
-				_id: mongoose.Types.ObjectId(user._id),
-			})
-			.lookup({
-				from: 'reviews',
-				localField: '_id',
-				foreignField: 'user',
-				as: 'userReviews',
-			})
-			.lookup({
-				from: 'orders',
-				localField: '_id',
-				foreignField: 'user',
-				as: 'userOrders',
-			})
-			.addFields({
-				totalReviews: { $size: '$userReviews' },
-				totalOrders: { $size: '$userOrders' },
-				token: token,
-			})
-			.append(pipeline);
+    const userDetails = User.aggregate()
+      .match({
+        _id: mongoose.Types.ObjectId(user._id),
+      })
+      .lookup({
+        from: "reviews",
+        localField: "_id",
+        foreignField: "user",
+        as: "userReviews",
+      })
+      .lookup({
+        from: "orders",
+        localField: "_id",
+        foreignField: "user",
+        as: "userOrders",
+      })
+      .addFields({
+        totalReviews: { $size: "$userReviews" },
+        totalOrders: { $size: "$userOrders" },
+        token: token,
+      })
+      .append(pipeline);
 
-		return userDetails;
-	} catch (err) {
-		return err;
-	}
+    return userDetails;
+  } catch (err) {
+    return err;
+  }
 };
 
 // Get Logged in User info
 const getLoggedInUser = async (userParam) => {
-	try {
-		const pipeline = [
-			{ $unset: ['userReviews', 'userOrders', 'cart', 'password', 'orders'] },
-		];
-		const user = User.aggregate()
-			.match({
-				_id: mongoose.Types.ObjectId(userParam),
-			})
-			.lookup({
-				from: 'reviews',
-				localField: '_id',
-				foreignField: 'user',
-				as: 'userReviews',
-			})
-			.lookup({
-				from: 'orders',
-				localField: '_id',
-				foreignField: 'user',
-				as: 'userOrders',
-			})
-			.addFields({
-				totalReviews: { $size: '$userReviews' },
-				totalOrders: { $size: '$userOrders' },
-			})
-			.append(pipeline);
+  try {
+    const pipeline = [
+      { $unset: ["userReviews", "userOrders", "cart", "password", "orders"] },
+    ];
+    const user = User.aggregate()
+      .match({
+        _id: mongoose.Types.ObjectId(userParam),
+      })
+      .lookup({
+        from: "reviews",
+        localField: "_id",
+        foreignField: "user",
+        as: "userReviews",
+      })
+      .lookup({
+        from: "orders",
+        localField: "_id",
+        foreignField: "user",
+        as: "userOrders",
+      })
+      .addFields({
+        totalReviews: { $size: "$userReviews" },
+        totalOrders: { $size: "$userOrders" },
+      })
+      .append(pipeline);
 
-		return user;
-	} catch (err) {
-		return err;
-	}
+    return user;
+  } catch (err) {
+    return err;
+  }
 };
 
 // Update User Details
 const updateUser = async (updateParam, id) => {
-	const { address, password, email, phone_number } = updateParam;
-	// Build User Object
-	const userFields = {};
+  const { address, password, email, phone_number } = updateParam;
+  // Build User Object
+  const userFields = {};
 
-	// Check for fields
-	if (address) userFields.address = address;
-	if (email) userFields.email = email;
-	if (phone_number) userFields.phone_number = phone_number;
+  // Check for fields
+  if (address) userFields.address = address;
+  if (email) userFields.email = email;
+  if (phone_number) userFields.phone_number = phone_number;
 
-	if (password) {
-		const salt = await bcrypt.genSalt(10);
+  if (password) {
+    const salt = await bcrypt.genSalt(10);
 
-		// Replace password from user object with encrypted one
-		userFields.password = await bcrypt.hash(password, salt);
-	}
+    // Replace password from user object with encrypted one
+    userFields.password = await bcrypt.hash(password, salt);
+  }
 
-	try {
-		// Find user from DB Collection
-		let user = await User.findById(id);
+  try {
+    // Find user from DB Collection
+    let user = await User.findById(id);
 
-		if (!user) throw { err: 'User not found' };
+    if (!user) throw { err: "User not found" };
 
-		// Updates the user Object with the changed values
-		user = await User.findByIdAndUpdate(
-			id,
-			{ $set: userFields },
-			{ omitUndefined: true, new: true, useFindAndModify: false }
-		);
+    // Updates the user Object with the changed values
+    user = await User.findByIdAndUpdate(
+      id,
+      { $set: userFields },
+      { omitUndefined: true, new: true, useFindAndModify: false }
+    );
 
-		user.cart = undefined;
-		user.password = undefined;
-		user.orders = undefined;
+    user.cart = undefined;
+    user.password = undefined;
+    user.orders = undefined;
 
-		return user;
-	} catch (err) {
-		return err;
-	}
+    return user;
+  } catch (err) {
+    return err;
+  }
 };
 
 const addItemToCart = async (userID, product) => {
-	try {
-		const productFinder = await Product.findById(product.product_id);
+  try {
+    const productFinder = await Product.findById(product.product_id);
 
-		if (!productFinder) throw { err: "can't find this product" };
+    if (!productFinder) throw { err: "can't find this product" };
 
-		let user = await User.findByIdAndUpdate(
-			userID,
-			{ $push: { cart: product } },
-			{ omitUndefined: true, new: true, useFindAndModify: false }
-		).populate({
-			path: 'cart.product_id',
-			select: 'product_name price availability',
-		});
+    let user = await User.findByIdAndUpdate(
+      userID,
+      { $push: { cart: product } },
+      { omitUndefined: true, new: true, useFindAndModify: false }
+    ).populate({
+      path: "cart.product_id",
+      select: "product_name price availability",
+    });
 
-		return user.cart;
-	} catch (error) {
-		return { err: 'error adding product to cart' };
-	}
+    return user.cart;
+  } catch (error) {
+    return { err: "error adding product to cart" };
+  }
 };
 
 const forgotPassword = async ({ email }) => {
-	try {
-		// verify if user exists, throws error if not
-		let findUser = await User.findOne({ email });
-		if (!findUser) throw { err: 'email is not registered' };
+  try {
+    // verify if user exists, throws error if not
+    let findUser = await User.findOne({ email });
+    if (!findUser) throw { err: "email is not registered" };
 
-		//Check if request has been made before, can be used for resend token
-		let oldTokenRequest = await Token.findOne({
-			email,
-			type: 'forget-password',
-		});
+    //Check if request has been made before, can be used for resend token
+    let oldTokenRequest = await Token.findOne({
+      email,
+      type: "forget-password",
+    });
 
-		if (oldTokenRequest) {
-			// resend old token
-			let email_subject = 'Password Reset Request';
-			await sendEmail(email, email_subject, oldTokenRequest.token);
+    if (oldTokenRequest) {
+      // resend old token
+      let email_subject = "Password Reset Request";
+      await sendEmail(email, email_subject, oldTokenRequest.token);
 
-			return 0;
-		}
+      return 0;
+    }
 
-		// generate OTP
-		let otp = otpGenerator.generate(4, {
-			alphabets: false,
-			upperCase: false,
-			specialChars: false,
-		});
+    // generate OTP
+    let otp = otpGenerator.generate(4, {
+      alphabets: false,
+      upperCase: false,
+      specialChars: false,
+    });
 
-		// add token to DB
-		let tokenData = {
-			email,
-			token: otp,
-			type: 'forgot-password',
-		};
+    // add token to DB
+    let tokenData = {
+      email,
+      token: otp,
+      type: "forgot-password",
+    };
 
-		await new Token(tokenData).save();
+    await new Token(tokenData).save();
 
-		// send otp
-		let email_subject = 'Password Reset Request';
-		let email_message = otp;
-		await sendEmail(email, email_subject, email_message);
+    // send otp
+    let email_subject = "Password Reset Request";
+    let email_message = otp;
+    await sendEmail(email, email_subject, email_message);
 
-		return 1;
-	} catch (err) {
-		return err;
-	}
+    return 1;
+  } catch (err) {
+    return err;
+  }
 };
 
 const validateToken = async ({ type, token, email }) => {
-	try {
-		// find token
-		let userToken = await Token.findOne({
-			token,
-			email,
-			type,
-		});
+  try {
+    // find token
+    let userToken = await Token.findOne({
+      token,
+      email,
+      type,
+    });
 
-		if (!userToken) throw { err: 'invalid token' };
+    if (!userToken) throw { err: "invalid token" };
 
-		return userToken;
-	} catch (err) {
-		return err;
-	}
+    return userToken;
+  } catch (err) {
+    return err;
+  }
 };
 
 const createNewPassword = async ({ email, password }) => {
-	try {
-		// validates token
-		let userToken = await Token.findOne({
-			email,
-			type: 'forgot-password',
-		});
+  try {
+    // validates token
+    let userToken = await Token.findOne({
+      email,
+      type: "forgot-password",
+    });
 
-		if (!userToken) throw { err: 'invalid token' };
+    if (!userToken) throw { err: "invalid token" };
 
-		// encrypting password
-		const salt = await bcrypt.genSalt(10);
-		password = await bcrypt.hash(password, salt);
+    // encrypting password
+    const salt = await bcrypt.genSalt(10);
+    password = await bcrypt.hash(password, salt);
 
-		// update new password
-		let user = await User.findOneAndUpdate(
-			{ email },
-			{ $set: { password } },
-			{ omitUndefined: true, new: true, useFindAndModify: false }
-		);
+    // update new password
+    let user = await User.findOneAndUpdate(
+      { email },
+      { $set: { password } },
+      { omitUndefined: true, new: true, useFindAndModify: false }
+    );
 
-		await Token.findByIdAndDelete(userToken._id);
+    await Token.findByIdAndDelete(userToken._id);
 
-		// Unsetting unneeded fields
-		user.cart = undefined;
-		user.password = undefined;
-		user.orders = undefined;
+    // Unsetting unneeded fields
+    user.cart = undefined;
+    user.password = undefined;
+    user.orders = undefined;
 
-		//send confirmation email
-		let email_subject = 'Password Reset Successful';
-		let email_message = 'Password has been reset successfully';
-		await sendEmail(email, email_subject, email_message);
+    //send confirmation email
+    let email_subject = "Password Reset Successful";
+    let email_message = "Password has been reset successfully";
+    await sendEmail(email, email_subject, email_message);
 
-		return user;
-	} catch (err) {
-		return err;
-	}
+    return user;
+  } catch (err) {
+    return err;
+  }
 };
 
 export {
-	getUsers,
-	registerUser,
-	loginUser,
-	getLoggedInUser,
-	updateUser,
-	addItemToCart,
-	forgotPassword,
-	validateToken,
-	createNewPassword,
+  getUsers,
+  verifyEmailAddress,
+  registerUser,
+  loginUser,
+  getLoggedInUser,
+  updateUser,
+  addItemToCart,
+  forgotPassword,
+  validateToken,
+  createNewPassword,
 };
