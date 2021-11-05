5ca98719aa0b0cc10943cec6121da0276f25db90
Obi James Onyemaechi <jamesobi@softprism.org>
2021-11-06 00:25:36 +0100

    fix: add isVerified property to user model

--- models/user.model.js
+++ models/user.model.js
@@ -1,27 +1,22 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const userSchema = mongoose.Schema({
-	first_name: { type: String, required: true },
-	last_name: { type: String, required: true },
-	phone_number: { type: String, required: true },
-	address: [{ 
-    address_type: {type: String, required: true },
-    value: {type: String, required: true} 
-  }],
-	email: { type: String, unique: true, required: true },
-	password: { type: String, required: true },
-	orders: [{ type: mongoose.Types.ObjectId, ref: 'Order' }], // array to store multiple orders
-  cart: [{
-    product_id: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'Product',
-      required: true,
+  first_name: { type: String, required: true },
+  last_name: { type: String, required: true },
+  phone_number: { type: String, required: true },
+  address: [
+    {
+      address_type: { type: String, required: true },
+      value: { type: String, required: true },
     },
-    quantity: { type: Number, required: true },
-  }], // array to store multiple products in cart
-	createdDate: { type: Date, default: Date.now },
+  ],
+  email: { type: String, unique: true, required: true },
+  password: { type: String, required: true },
+  orders: [{ type: mongoose.Types.ObjectId, ref: "Order" }], // array to store multiple orders
+  isVerified: { type: Boolean, required: true, default: false },
+  createdDate: { type: Date, default: Date.now },
 });
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 
 export default User;
