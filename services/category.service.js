730e532ba640ace37a80d3eeb5cfe817b333aff7
Obi James Onyemaechi <jamesobi@softprism.org>
2021-11-07 02:44:23 +0100

    feat: add location filter on category
    for now users will only get categories available to them in certain locations, ie only stores in categories closer to them will be shown

--- services/category.service.js
+++ services/category.service.js
@@ -1,126 +1,136 @@
-import Category from '../models/category.model.js'
+import Category from "../models/category.model.js";
 
 //  Get all Categories
 const getCategories = async (urlParams) => {
-	try {
+  try {
     const limit = Number(urlParams.limit);
-		const skip = Number(urlParams.skip);
-    const pipeline = [{ 
-      $unset: ['products', 'stores']
-    }];
-    return Category.aggregate()
-    .lookup({
-      from: 'products',
-      localField: '_id',
-      foreignField: 'category',
-      as: 'products'
-    })
-    .lookup({
-      from: 'stores',
-      localField: '_id',
-      foreignField: 'category',
-      as: 'stores'
-    })
-    .addFields({
-      productCount: {$size: '$products'}
-    })
-    .addFields({
-      storeCount: {$size: '$stores'}
-    })
-    .append(pipeline);
+    const skip = Number(urlParams.skip);
 
-	} catch (err) {
-		return err;
-	}
+    if (urlParams.long && urlParams.lat && urlParams.radius) {
+      var long = parseFloat(urlParams.long);
+      var lat = parseFloat(urlParams.lat);
+      var radian = parseFloat(urlParams.radius / 3963.2);
+    }
+
+    const pipeline = [
+      {
+        $unset: ["products", "stores"],
+      },
+    ];
+    return Category.aggregate()
+      .lookup({
+        from: "products",
+        localField: "_id",
+        foreignField: "category",
+        as: "products",
+      })
+      .lookup({
+        from: "stores",
+        localField: "_id",
+        foreignField: "category",
+        as: "stores",
+      })
+      .match({
+        "stores.location": {
+          $geoWithin: {
+            $centerSphere: [[long, lat], radian],
+          },
+        },
+      })
+      .addFields({
+        productCount: { $size: "$products" },
+      })
+      .addFields({
+        storeCount: { $size: "$stores" },
+      })
+      .append(pipeline);
+  } catch (err) {
+    return err;
+  }
 };
 
 // Create a new Category
 const createCategory = async (categoryParams) => {
-	const { name, image } = categoryParams;
+  const { name, image } = categoryParams;
 
-	try {
-		// Check if Category exists
-		let category = await Category.findOne({ name });
+  try {
+    // Check if Category exists
+    let category = await Category.findOne({ name });
 
-		if (category) {
-			throw { err: `${category.name} already Exists` };
-		}
+    if (category) {
+      throw { err: `${category.name} already Exists` };
+    }
 
-		// Create Category Object
-		category = new Category({
-			name,
-			image,
-		});
+    // Create Category Object
+    category = new Category({
+      name,
+      image,
+    });
 
-		// Save Category to db
-		await category.save();
+    // Save Category to db
+    await category.save();
 
-		return {msg: `${category.name} category successfully created`};
-	} catch (err) {
-		return err;
-	}
+    return { msg: `${category.name} category successfully created` };
+  } catch (err) {
+    return err;
+  }
 };
 
 // Edit a category
 const editCategory = async (editParams, id) => {
-	// const { name, image } = editParams;
+  // const { name, image } = editParams;
 
-	// // Build Category Object
-	// const categoryFields = {};
+  // // Build Category Object
+  // const categoryFields = {};
 
-	// // Check for fields
-	// if (image) categoryFields.name = image;
-	// if (name) categoryFields.name = name;
-  
-	try {
-		let category = await Category.findById(id);
+  // // Check for fields
+  // if (image) categoryFields.name = image;
+  // if (name) categoryFields.name = name;
 
-		if (!category) throw { err: 'Category not found' };
+  try {
+    let category = await Category.findById(id);
+
+    if (!category) throw { err: "Category not found" };
 
     // Images should be replaced
 
-		// Check if image field is not empty
-		// if (image !== '' || null) {
-		// 	// Check if image array is not empty
-		// 	if (!category.image.length < 1) {
-		// 		// Set the image string value in category object to image found from db, then append new image string
-		// 		categoryFields.image = [...category.image, image];
-		// 	}
-		// }
+    // Check if image field is not empty
+    // if (image !== '' || null) {
+    // 	// Check if image array is not empty
+    // 	if (!category.image.length < 1) {
+    // 		// Set the image string value in category object to image found from db, then append new image string
+    // 		categoryFields.image = [...category.image, image];
+    // 	}
+    // }
 
-		// Updates the user Object with the changed values
-		category = await Category.findByIdAndUpdate(
-			id,
-			{ $set: editParams },
-			{ omitUndefined: true, new: true, useFindAndModify: false }
-		);
+    // Updates the user Object with the changed values
+    category = await Category.findByIdAndUpdate(
+      id,
+      { $set: editParams },
+      { omitUndefined: true, new: true, useFindAndModify: false }
+    );
 
-		return category;
-	} catch (err) {
-		return err;
-	}
+    return category;
+  } catch (err) {
+    return err;
+  }
 };
 
 //   Delete a Category
 const deleteCategory = async (id) => {
-	try {
-		// Check if id matches any category from db
-		let category = await Category.findById(id);
+  try {
+    // Check if id matches any category from db
+    let category = await Category.findById(id);
 
-		if (!category) throw { err: 'Category not found' };
+    if (!category) throw { err: "Category not found" };
 
-		// Remove category by id
-		await Category.findByIdAndRemove(id);
+    // Remove category by id
+    await Category.findByIdAndRemove(id);
 
-		return { msg: 'Category Deleted' };
-	} catch (err) {
-		return err;
-	}
+    return { msg: "Category Deleted" };
+  } catch (err) {
+    return err;
+  }
 };
 
-export {
-	getCategories,
-	createCategory,
-	editCategory,
-	deleteCategory,
-};
+export { getCategories, createCategory, editCategory, deleteCategory };
