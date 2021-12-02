d618b63f4a23af74ae327311934f47b744881f9f
Obi James Onyemaechi <jamesobi@softprism.org>
2021-12-03 00:32:10 +0100

    fix: improve error message

--- middleware/checkPagination.js
+++ middleware/checkPagination.js
@@ -2,7 +2,7 @@ const checkPagination = (req, res, next) => {
   if (req.query.skip === undefined || req.query.limit === undefined) {
     return res
       .status(400)
-      .json({ success: false, msg: "filtering parameters are missing" });
+      .json({ success: false, msg: "pagination parameters are missing" });
   }
   next();
 };
