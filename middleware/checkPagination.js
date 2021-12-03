ac06e21b260705bd39b009d3b5b58e334ad4b6c7
Obi James Onyemaechi <jamesobi@softprism.org>
2021-12-03 19:21:06 +0100

    feat: improve pagination

--- middleware/checkPagination.js
+++ middleware/checkPagination.js
@@ -1,9 +1,10 @@
 const checkPagination = (req, res, next) => {
-  if (req.query.skip === undefined || req.query.limit === undefined) {
+  if (req.query.page === undefined || req.query.limit === undefined) {
     return res
       .status(400)
       .json({ success: false, msg: "pagination parameters are missing" });
   }
+  req.query.skip = (req.query.page - 1) * req.query.limit;
   next();
 };
 
