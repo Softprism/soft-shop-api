1e7ab782609178389b45f1a17cc6b46e2848d9c0
Obi James Onyemaechi <jamesobi@softprism.org>
2021-12-06 23:52:02 +0100

    fix: return categories without stores
    previous function returned no category when it can't find a store.

--- services/category.service.js
+++ services/category.service.js
@@ -9,26 +9,29 @@ const getCategories = async (urlParams) => {
       var radian = parseFloat(urlParams.radius / 3963.2);
     }
 
+    // declare fields to remove after aggregating
     const pipeline = [
       {
-        $unset: ["products"],
+        $unset: ["stores"],
       },
     ];
+
+    // start aggregate
     return (
       Category.aggregate()
-        .lookup({
-          from: "products",
-          localField: "_id",
-          foreignField: "category",
-          as: "products",
-        })
+        // lookup stores - trying to JOIN the categories and stores
         .lookup({
           from: "stores",
-          localField: "_id",
-          foreignField: "category",
+          // declare local variable for category id, since we're using version 4 of mongodb we can't use foreign and local fields here, hence doing it the OG way.
+          let: { category_id: "$_id" },
           pipeline: [
             {
+              // matching each category to a store, this should return all categories and a store array field inside each category object, an empty array if the category has no stores.
               $match: {
+                $expr: {
+                  $eq: ["$$category_id", "$category"],
+                },
+                // run geoWithin operation to return all stores that are within the selected radius of the user's location
                 location: {
                   $geoWithin: {
                     $centerSphere: [[long, lat], radian],
@@ -39,20 +42,11 @@ const getCategories = async (urlParams) => {
           ],
           as: "stores",
         })
-        // .unwind({ path: "$stores", preserveNullAndEmptyArrays: true })
-        .match({
-          "stores.location": {
-            $geoWithin: {
-              $centerSphere: [[long, lat], radian],
-            },
-          },
-        })
-        .addFields({
-          productCount: { $size: "$products" },
-        })
+        // count number of stores in each category, will return zero for empty arrays
         .addFields({
           storeCount: { $size: "$stores" },
         })
+        // appends the pipeline specified above
         .append(pipeline)
     );
   } catch (err) {
