4fb343313741a032c60d5e80bd2fda9eed76a14c
Obi James Onyemaechi <jamesobi@softprism.org>
2021-12-09 02:29:58 +0100

    fix: minor improvement

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import { errorHandler } from './middleware/errorMiddleware.js';
-import cors from 'cors';
-import { connectDB } from './config/db.js';
-import userRoutes from './routes/user.routes.js';
-import adminRoutes from './routes/admin.routes.js';
-import productRoutes from './routes/product.routes.js';
-import storeRoutes from './routes/store.routes.js';
-import orderRoutes from './routes/order.routes.js';
-import categoryRoutes from './routes/category.routes.js'
+import express from "express";
+import dotenv from "dotenv";
+import { errorHandler } from "./middleware/errorMiddleware.js";
+import cors from "cors";
+import { connectDB } from "./config/db.js";
+import userRoutes from "./routes/user.routes.js";
+import adminRoutes from "./routes/admin.routes.js";
+import productRoutes from "./routes/product.routes.js";
+import storeRoutes from "./routes/store.routes.js";
+import orderRoutes from "./routes/order.routes.js";
+import categoryRoutes from "./routes/category.routes.js";
 
 dotenv.config();
 
@@ -21,14 +21,17 @@ app.use(express.json());
 app.use(cors());
 
 // api routes
-app.use('/users', userRoutes);
-app.use('/admins', adminRoutes);
-app.use('/products', productRoutes);
-app.use('/stores', storeRoutes);
-app.use('/order', orderRoutes);
-app.use('/category', categoryRoutes);
-app.get('/', (req, res) => {
-	res.status(200).json({ success: true, msg: 'API is running...'} );
+app.use("/users", userRoutes);
+app.use("/admins", adminRoutes);
+app.use("/products", productRoutes);
+app.use("/stores", storeRoutes);
+app.use("/order", orderRoutes);
+app.use("/category", categoryRoutes);
+app.all("*", (req, res, next) => {
+  res
+    .status(200)
+    .json({ success: true, msg: "server is fine. Jazz up if you're lost" });
+  next();
 });
 
 // global error handler
@@ -38,6 +41,6 @@ app.use(errorHandler);
 const PORT = process.env.PORT || 5000;
 
 app.listen(
-	PORT,
-	console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 );
