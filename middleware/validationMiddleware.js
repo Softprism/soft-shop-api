cc825175e252f082ffb7d37a62906f0a8213307e
Obi James Onyemaechi <jamesobi@softprism.org>
2021-12-17 02:23:19 +0100

    feat: create validationMiddleware to do all validations on inputs before sending it to routes

---
+++ middleware/validationMiddleware.js
@@ -0,0 +1,18 @@
+import validator from "validator";
+
+const userLogin = async (req, res, next) => {
+  if (!validator.isEmail(req.body.email)) {
+    return res.status(400).json({
+      success: false,
+      msg: "email entered is invalid, please try again",
+    });
+  } else if (validator.isEmpty(req.body.password)) {
+    return res.status(400).json({
+      success: false,
+      msg: "Password field is missing!",
+    });
+  }
+  next();
+};
+
+export { userLogin };
