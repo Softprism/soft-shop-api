c00c1c2701844fa31b7591a716903c4649b78db5
Obi James Onyemaechi <jamesobi@softprism.org>
2021-12-20 02:01:30 +0100

    chore: improve auth middleware

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,29 +1,42 @@
 import jwt from "jsonwebtoken";
+import User from "../models/user.model";
+import Admin from "../models/admin.model";
+import Store from "../models/store.model";
 
-const auth = (req, res, next) => {
+const auth = async (req, res, next) => {
+  let token;
+
   if (
     req.headers.authorization && req.headers.authorization.startsWith("Bearer")
   ) {
     try {
-      const token = req.headers.authorization.split(" ")[1];
+      // eslint-disable-next-line prefer-destructuring
+      token = req.headers.authorization.split(" ")[1];
 
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
       if (decoded.user) {
+        const { user } = decoded;
+        let verifyUser = await User.findById(user.id);
+        if (!verifyUser) res.status(401).json({ success: false, msg: "user doesn't exist" });
         req.user = decoded.user;
       }
 
       if (decoded.store) {
+        const { store } = decoded;
+        let verifyStore = await Store.findById(store.id);
+        if (!verifyStore) res.status(401).json({ success: false, msg: "store doesn't exist" });
         req.store = decoded.store;
       }
 
       if (decoded.admin) {
+        const { admin } = decoded;
+        let verifyAdmin = await Store.findById(admin.id);
+        if (!verifyAdmin) res.status(401).json({ success: false, msg: "admin doesn't exist" });
         req.admin = decoded.admin;
       }
 
       next();
     } catch (err) {
-      console.error(err);
       res
         .status(401)
         .json({ success: false, msg: "Not authorized, token failed" });
