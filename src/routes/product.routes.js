088a4c2eb8f5ffa56817ecb2579c48726c62cba4
Obi James Onyemaechi <jamesobi@softprism.org>
2021-12-23 18:19:43 +0100

    chore: minor improvements

--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -1,7 +1,7 @@
 import express from "express";
 import { check, validationResult } from "express-validator";
 import auth from "../middleware/auth";
-import isStoreAdmin from "../middleware/verifyStorePermission";
+import { isStoreAdmin } from "../middleware/Permissions";
 import checkPagination from "../middleware/checkPagination";
 
 import {
