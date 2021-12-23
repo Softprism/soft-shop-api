6f1a3e4892184f7d31a733171dfd1e29a7bbfece
Obi James Onyemaechi <jamesobi@softprism.org>
2021-12-23 02:09:42 +0100

    chore: improve error handling for admin

--- src/services/admin.service.js
+++ src/services/admin.service.js
@@ -4,108 +4,87 @@ import bcrypt from "bcryptjs";
 import Admin from "../models/admin.model";
 
 const getAdmins = async () => {
-  try {
-    const admins = await Admin.find();
-
-    if (admins.length > 0) {
-      return admins;
-    }
-  } catch (err) {
-    return err;
-  }
+  const admins = await Admin.find();
+  return admins;
 };
 
 const registerAdmin = async (params) => {
   const { username, password } = params;
 
-  try {
-    let admin = await Admin.findOne({ username });
+  let admin = await Admin.findOne({ username });
 
-    if (admin) {
-      throw { err: "Admin account already exists." };
-    }
-
-    // Create Admin Object
-    admin = new Admin({
-      username,
-      password,
-    });
-
-    const salt = await bcrypt.genSalt(10);
-
-    // Replace password from user object with encrypted one
-    admin.password = await bcrypt.hash(password, salt);
-
-    // Save user to db
-    await admin.save();
-
-    // Define payload for token
-    const payload = {
-      admin: {
-        id: admin.id,
-      },
-    };
-
-    // Generate and return token to server
-    const token = jwt.sign(payload, process.env.JWT_SECRET, {
-      expiresIn: 36000,
-    });
-
-    return token;
-  } catch (err) {
-    return err;
+  if (admin) {
+    return { err: "Admin account already exists.", status: 409 };
   }
+
+  // Create Admin Object
+  admin = new Admin({
+    username,
+    password,
+  });
+
+  const salt = await bcrypt.genSalt(10);
+
+  // Replace password from user object with encrypted one
+  admin.password = await bcrypt.hash(password, salt);
+
+  // Save user to db
+  await admin.save();
+
+  // Define payload for token
+  const payload = {
+    admin: {
+      id: admin.id,
+    },
+  };
+
+  // Generate and return token to server
+  const token = jwt.sign(payload, process.env.JWT_SECRET, {
+    expiresIn: 36000,
+  });
+
+  return token;
 };
 
 const loginAdmin = async (loginParam) => {
   const { username, password } = loginParam;
+  // Find admin with email
+  let admin = await Admin.findOne({ username });
 
-  try {
-    // Find admin with email
-    let admin = await Admin.findOne({ username });
-
-    if (!admin) {
-      throw { err: "Admin not found." };
-    }
-
-    // Check if password matches with stored hash
-    const isMatch = await bcrypt.compare(password, admin.password);
-
-    if (!isMatch) {
-      throw { err: "The password entered is invalid, please try again." };
-    }
-
-    // Define payload for token
-    const payload = {
-      admin: {
-        id: admin.id,
-      },
-    };
-
-    // Generate and return token to server
-    const token = jwt.sign(payload, process.env.JWT_SECRET, {
-      expiresIn: 36000,
-    });
-
-    if (!token) {
-      throw { err: "Missing Token" };
-    }
-
-    return token;
-  } catch (err) {
-    console.log(err);
-    return err;
+  if (!admin) {
+    return { err: "Admin not found.", status: 404 };
   }
+
+  // Check if password matches with stored hash
+  const isMatch = await bcrypt.compare(password, admin.password);
+
+  if (!isMatch) {
+    return { err: "The password entered is invalid, please try again.", status: 401 };
+  }
+
+  // Define payload for token
+  const payload = {
+    admin: {
+      id: admin.id,
+    },
+  };
+
+  // Generate and return token to server
+  const token = jwt.sign(payload, process.env.JWT_SECRET, {
+    expiresIn: 36000,
+  });
+
+  if (!token) {
+    return { err: "Missing Token", status: 400 };
+  }
+
+  return token;
 };
 
 const getLoggedInAdmin = async (adminParam) => {
-  try {
-    const admin = await Admin.findById(adminParam).select("-password");
+  const admin = await Admin.findById(adminParam).select("-password");
 
-    return admin;
-  } catch (err) {
-    return err;
-  }
+  return admin;
 };
 
 const updateAdmin = async (updateParam, id) => {
@@ -127,7 +106,7 @@ const updateAdmin = async (updateParam, id) => {
     // Find admin from DB Collection
     let admin = await Admin.findById(id);
 
-    if (!admin) throw { msg: "Admin not found." };
+    if (!admin) return { msg: "Admin not found.", status: 404 };
 
     // Updates the admin Object with the changed values
     admin = await Admin.findByIdAndUpdate(
