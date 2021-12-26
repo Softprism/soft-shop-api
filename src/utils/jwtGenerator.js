cc83aa62a185df59d820bea1965e552e0e1a6668
Obi James Onyemaechi <jamesobi@softprism.org>
2021-12-26 23:51:17 +0100

    feat: add jwt generator for auth processes

---
+++ src/utils/jwtGenerator.js
@@ -0,0 +1,38 @@
+import jwt from "jsonwebtoken";
+
+const getJwt = async (id, type) => {
+  let payload;
+  if (type === "store") {
+    payload = {
+      store: {
+        id,
+      },
+    };
+  }
+  if (type === "user") {
+    payload = {
+      user: {
+        id,
+      },
+    };
+  }
+  if (type === "admin") {
+    payload = {
+      admin: {
+        id,
+      },
+    };
+  }
+
+  // Generate and return token to server
+  const token = jwt.sign(payload, process.env.JWT_SECRET, {
+    expiresIn: "365 days",
+  });
+
+  if (!token) {
+    throw { err: "Missing Token.", status: 400 };
+  }
+  return token;
+};
+
+export default getJwt;
