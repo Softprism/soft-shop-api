aa9ba51b72f519b4a64ba5a50b64770aa34713b3
Obi James Onyemaechi <jamesobi@softprism.org>
2022-01-28 22:50:33 +0100

    feat: reverse geocoding

--- src/utils/geocode-address.js
+++ src/utils/geocode-address.js
@@ -2,18 +2,45 @@ import { Client } from "@googlemaps/google-maps-services-js";
 
 const client = new Client({});
 const getGeocode = async (req, res, next) => {
-  if (!req.query.place_id) return res.status(400).json({ success: false, msg: "Please enter an address.", status: 400 });
+  let geoCodeParams = {};
+
+  if (req.query.place_id) {
+    geoCodeParams.place_id = req.query.place_id;
+    geoCodeParams.key = process.env.GOOGLE_MAPS_API_KEY;
+  } else if (req.query.latlng) {
+    geoCodeParams.latlng = req.query.latlng;
+    geoCodeParams.type = "street_address";
+    geoCodeParams.key = process.env.GOOGLE_MAPS_API_KEY;
+  } else {
+    return res.status(400).json({ success: false, msg: "Please enter a place_id or latlng value for this request.", status: 400 });
+  }
+
+  if (req.query.place_id && req.query.latlng) {
+    return res.status(400).json({ success: false, msg: "You can only use this endpoint to geocode or reverse geocode dear.", status: 400 });
+  }
 
   try {
     let geocode = await client.geocode({
       params: {
-        place_id: req.query.place_id,
-        key: process.env.GOOGLE_MAPS_API_KEY
+        ...geoCodeParams
       },
-      timeout: 1000
+      timeout: 10000
     });
-    return res.status(200).json({ success: true, result: geocode.data.results[0].geometry, status: 200 });
+    if (req.query.place_id) {
+      return res.status(200).json({ success: true, result: geocode.data.results[0].geometry.location, status: 200 });
+    }
+    if (req.query.latlng) {
+      return res.status(200).json({
+        success: true,
+        result: {
+          address: geocode.data.results[0].formatted_address,
+          place_id: geocode.data.results[0].place_id
+        },
+        status: 200
+      });
+    }
   } catch (error) {
+    console.log(error);
     return res.status(400).json({ success: false, msg: error.response.data.status, status: 400 });
   }
 };
