1116d93a35daf7a62cba7323483a44f81f6a9556
Obi James Onyemaechi <jamesobi@softprism.org>
2022-02-01 00:31:50 +0100

    chore: integrating with cpanel

--- src/routes/test.routes.js
+++ src/routes/test.routes.js
@@ -6,6 +6,7 @@ router.get("/", (req, res) => {
   return res.status(200).json({
     success: true,
     result: "API is working!",
+    env: process.env.GOOGLE_MAPS_API_KEY,
     status: 200
   });
 });
