242d68447db89ca4b79732de20ba464a040c451b
Obi James Onyemaechi <jamesobi@softprism.org>
2022-03-20 23:21:29 +0100

    chore: setting server time

--- src/routes/test.routes.js
+++ src/routes/test.routes.js
@@ -4,13 +4,12 @@ import { signupLog } from "../services/logs.service";
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-  next();
   return res.status(200).json({
     success: true,
-    result: `API is working! Socket for push notifications ${new Date().toISOString()}`,
+    result: `API is working! Socket for push notifications ${new Date().toString()}`,
     env: process.env.GOOGLE_MAPS_API_KEY,
     status: 200
   });
-}, signupLog);
+});
 
 export default router;
