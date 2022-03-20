f9c2bc8688cf2e47e8562937136889a853de7c02
Obi James Onyemaechi <jamesobi@softprism.org>
2022-03-20 13:14:59 +0100

    feat: push notifications

--- src/routes/test.routes.js
+++ src/routes/test.routes.js
@@ -7,7 +7,7 @@ router.get("/", (req, res, next) => {
   next();
   return res.status(200).json({
     success: true,
-    result: "API is working!",
+    result: "API is working! Socket for push notifications",
     env: process.env.GOOGLE_MAPS_API_KEY,
     status: 200
   });
