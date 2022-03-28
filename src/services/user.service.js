b511cf2705e1c22a310df8af9be27fc0a69e9af6
Obi James Onyemaechi <jamesobi@softprism.org>
2022-03-28 15:53:48 +0100

    chore: minor addition

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -101,7 +101,7 @@ const registerUser = async (userParam) => {
   let newUser = await user.save();
 
   // delete sign up token
-  // await Token.findByIdAndDelete(userParam.token);
+  await Token.findByIdAndDelete(userParam.token);
 
   // delete user on creation, uncomment to test registration without populating your database
   // await User.findByIdAndDelete(newUser._id);
