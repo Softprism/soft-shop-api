61b5b7ad1234b79a5e12f844a51f9049d5d845f5
Obi James Onyemaechi <jamesobi@softprism.org>
2022-03-29 23:24:16 +0100

    chore: add getdistance endpoint service

--- src/routes/services.routes.js
+++ src/routes/services.routes.js
@@ -6,7 +6,7 @@ import validator from "../middleware/validator";
 import geoCodeValidation from "../validations/geoCodeValidation";
 
 import auth from "../middleware/auth";
-import getDistance from "../utils/get-distance";
+import { getDistanceService } from "../utils/get-distance";
 
 const router = express.Router();
 
@@ -23,6 +23,6 @@ router.get("/geocode", validator(geoCodeValidation), getGeocode);
 // @route   GET /distance
 // @desc    Get distance from user to store
 // @access  Private
-router.get("/distance", getDistance);
+router.get("/distance", getDistanceService);
 
 export default router;
