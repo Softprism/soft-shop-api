f3dca48aa04eda3708dfbceeb98973e896aa1cdd
Obi James Onyemaechi <jamesobi@softprism.org>
2022-04-09 01:22:27 +0100

    chore: minor improvement

--- src/controllers/order.controller.js
+++ src/controllers/order.controller.js
@@ -89,7 +89,7 @@ const getOrderDetails = async (req, res, next) => {
 
     if (orderDetails.err) return res.status(orderDetails.status).json({ success: false, msg: orderDetails.err, status: orderDetails.status });
 
-    return res.status(200).json({ success: true, result: orderDetails[0] });
+    return res.status(200).json({ success: true, result: orderDetails });
   } catch (error) {
     next(error);
   }
